ef8831715dc6eefe4ca5155f56b7e0ed94b348c8
hansenjl <jenn.leigh.hansen@gmail.com>
2021-02-02 06:50:27 -0800

    remove fetch

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-const baseUrl = 'https://www.themealdb.com/api/json/v1/1/search.php'
-fetch(baseUrl + '?s=chicken')
-.then(res => {
-    console.log(res)
-    let newRes = res.json()
-    console.log(newRes)
-    return newRes
-}).then(data => {
-    console.log(data)
-})
+const baseUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s='
+
